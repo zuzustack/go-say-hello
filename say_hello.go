3c836b76e25732bcd81ffef774cb79453664e7eb
zuzustack <ridwanyamada@gmail.com>
2022-10-28 00:57:53 +0700

    update

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,15 @@
 package go_say_hello
 
-func Say() string{
+import "fmt"
+
+func Say() string {
 	return "Hello"
+}
+
+func SayWorld() string {
+	return "Hello World"
+}
+
+func SayName(name string ) string {
+	return fmt.Sprint("Hello %v",name)
 }
